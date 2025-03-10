454ef5651bfe7fe9beede1259832dc5612fc2c59
Jose Walter <jwdutra@gmail.com>
2025-03-10 20:51:50 -0300

    Refatora a função main para melhorar a inicialização e a estrutura do código

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -5,25 +5,20 @@ import 'package:core/core.dart';
 import 'src/app.dart';
 
 Future<void> main() async {
-  runZonedGuarded<Future<void>>(() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-    await Environment.initialize();
+      await Environment.initialize();
 
-    var hiveBoxPath = 'nasa_app';
-    if (Environment.platform != 'web') {
-      var appDocumentDirectory = await getApplicationDocumentsDirectory();
-      hiveBoxPath = '${appDocumentDirectory.path}/nasa_app';
-    }
-    Hive.init(hiveBoxPath);
+      /// Inicializa os bindings
+      AppBindings bindings = AppBindings();
+      bindings.dependencies();
 
-    /// Inicializa os bindings
-    AppBindings bindings = AppBindings();
-    bindings.dependencies();
-
-    runApp(App());
-  }, (error, stackTrace) {
-    //SentryObserver.captureException(error, stackTrace: stackTrace);
-    //FirebaseCrashlytics.instance.recordError(error, stackTrace);
-  });
+      runApp(App());
+    },
+    (error, stackTrace) {
+      // Ferramentas de observabilidade como Sentry e Crashlytics
+    },
+  );
 }
